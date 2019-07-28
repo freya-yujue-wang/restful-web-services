1fed968eeea21a63c3c3364890e6f25f9b622b91
wyjsusan <yujuewa@gmail.com>
2019-07-28 16:26:36 -0700

    Controller Method with Path Variables.

--- src/main/java/com/amituofo/spring/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/amituofo/spring/restfulwebservices/controllers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.amituofo.spring.restfulwebservices.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,11 @@ public class HelloWorldController {
   @RequestMapping(method = RequestMethod.GET, path = "/hello-world-bean")
   public HelloWorldBean helloWorldBean() {
     return new HelloWorldBean("Hello World");
+  }
+
+  //hello-world/path-variable
+  @RequestMapping(method = RequestMethod.GET, path = "/hello-world/path-variable/{name}")
+  public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+    return new HelloWorldBean(String.format("Hello World, %s", name));
   }
 }
