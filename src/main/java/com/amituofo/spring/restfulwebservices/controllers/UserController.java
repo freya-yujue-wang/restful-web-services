0638f6b1af68fdfab3146a734bbb94e008b9373f
wyjsusan <yujuewa@gmail.com>
2019-08-01 23:00:50 -0700

    Implement HATEOAS for RESTful services.

--- src/main/java/com/amituofo/spring/restfulwebservices/controllers/UserController.java
+++ src/main/java/com/amituofo/spring/restfulwebservices/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.amituofo.spring.restfulwebservices.controllers;
 
-import com.amituofo.spring.restfulwebservices.services.UserService;
 import com.amituofo.spring.restfulwebservices.models.User;
+import com.amituofo.spring.restfulwebservices.services.UserService;
 import java.net.URI;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 
 @RestController
@@ -30,12 +34,21 @@ public class UserController {
 
   //retrieveUser(int id)
   @GetMapping("/users/{id}")
-  public User retrieveUser(@PathVariable int id) {
+  public Resource<User> retrieveUser(@PathVariable int id) {
     User user = _userService.findOne(id);
     if (user == null) {
       throw new UserNotFoundException("id-" + id);
     }
-    return user;
+
+    //"all-user", SERVER_PATH + "/users/"
+    //retrieveAllUsers
+    Resource<User> resource = new Resource<>(user);
+
+    ControllerLinkBuilder linkBuilder = linkTo(methodOn(this.getClass()).retrieveALlUsers());
+
+    resource.add(linkBuilder.withRel("all-users"));
+
+    return resource;
   }
 
   //created
