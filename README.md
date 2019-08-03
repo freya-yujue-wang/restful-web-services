25de01ca15685cac29386403f3c0919aae8de2a1
wyjsusan <yujuewa@gmail.com>
2019-08-02 21:36:17 -0700

    Internationalization for RESTful services.

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ User -> Posts
 - Retrieve all posts for a User   - GET /users/{id}/posts
 - Create a posts for a User       - POST /users/{id}/posts
 - Retrieve details of a post      - GET /users/{id}/posts/{post_id}
+
+
+#### Internationalization
+
+##### Configuration
+- LocaleResolver
+  - Default Locale - Locale.US
+- ResourceBundleMessageSource
+
+##### Usage
+- Autowire MessageSource
+- @RequestHeader(value = "Accept-Language", required = false) Locale locale
+- messageSource.getMessage("helloWorld.message", null, locale)
--- src/main/java/com/amituofo/spring/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/amituofo/spring/restfulwebservices/controllers/HelloWorldController.java
@@ -1,6 +1,10 @@
 package com.amituofo.spring.restfulwebservices.controllers;
 
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+  @Autowired
+  private MessageSource messageSource;
 
   @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
   public String helloWorld() {
@@ -24,5 +31,10 @@ public class HelloWorldController {
   @RequestMapping(method = RequestMethod.GET, path = "/hello-world/path-variable/{name}")
   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
     return new HelloWorldBean(String.format("Hello World, %s", name));
+  }
+
+  @RequestMapping(method = RequestMethod.GET, path = "/hello-world-i18n")
+  public String helloWorldI18n(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+    return messageSource.getMessage("good.morning.message", null, locale);
   }
 }
