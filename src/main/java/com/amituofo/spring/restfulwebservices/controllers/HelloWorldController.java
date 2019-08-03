3522adb9044aebcdc1c05f84168c910a1b8f04ad
wyjsusan <yujuewa@gmail.com>
2019-08-02 21:52:21 -0700

    Internationalization for RESTful services - Improvement.

--- src/main/java/com/amituofo/spring/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/amituofo/spring/restfulwebservices/controllers/HelloWorldController.java
@@ -1,10 +1,9 @@
 package com.amituofo.spring.restfulwebservices.controllers;
 
-import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +33,7 @@ public class HelloWorldController {
   }
 
   @RequestMapping(method = RequestMethod.GET, path = "/hello-world-i18n")
-  public String helloWorldI18n(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-    return messageSource.getMessage("good.morning.message", null, locale);
+  public String helloWorldI18n() {
+    return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
   }
 }
